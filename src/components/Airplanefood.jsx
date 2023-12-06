c1a7ca66c1e91fd4c9e81c06ddd3a173ac81377f
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-12-06 11:44:05 -0600

    29 commit

--- src/components/Airplanefood.jsx
+++ src/components/Airplanefood.jsx
@@ -4,7 +4,7 @@ import "./Airplanefood.css";
 function Airplanefood() {
   return (
     <div>
-      <h1 className='food-title'>Serve what you Deserve</h1>
+      <h1 className='food-title'>SERVE WHAT YOU DESERVE</h1>
       <div className='food-list-container'>
         <ul>
           <li><img src="/food1.jpg" alt="food 1" /></li>
