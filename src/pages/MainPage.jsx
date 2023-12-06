79f9490eb07b638aa9a7d68b3c2620d0caf654ff
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-12-06 17:29:20 -0600

    37 commit

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -15,7 +15,7 @@ const MainPage = () => {
         {/* the container for my images*/}
         <div className="imagesContainer">
           <div className="title1image">
-            <h2>Each airport is piece of Art</h2>
+            <h2>Aviation Nexus</h2>
           </div>
 
           <div className="title2image">
@@ -23,15 +23,15 @@ const MainPage = () => {
               src="trackingstatus.png"
               style={{ width: "150px", height: "150px" }}
             />
-            <h2>Fly High and Discover the World</h2>
+            <h2>Soaring Elevation</h2>
           </div>
 
           <div className="title3image">
-            <h2>Between Airlines and Airplanes is Story</h2>
+            <h2>Enjoy the Sky</h2>
           </div>
 
           <div className="title4image">
-            <h2>Visit each Country Safely</h2>
+            <h2>Visit the World</h2>
           </div>
         </div>
 
