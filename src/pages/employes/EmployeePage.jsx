35411ac5b5f98d7281b74ee7d7aec3720937265a
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-12-09 16:14:09 -0600

    58 commit

--- src/pages/employes/EmployeePage.jsx
+++ src/pages/employes/EmployeePage.jsx
@@ -10,24 +10,24 @@ function EmployeePage() {
       <Header className="headerTitle" txt={"aviation Info"} />
       <h2 className="PageName">{worker.name}</h2>
       <img className="ProfileImageRight" src={worker.image} alt={worker.name} />
-      <h3 className = "text-my-h3">
-        Position: <strong>{worker.position}</strong>
-      </h3>
-      <h3 className = "text-my-h3">
-        Duty: <strong>{worker.duty}</strong>
-      </h3>
-      <h3 className = "text-my-h3">
-        Field: <strong>{worker.field}</strong>
-      </h3>
-      <h3 className = "text-my-h3">
-        Average Salary: <strong>{worker.averageSalary}</strong>
-      </h3>
-      <h3 className = "text-my-h3">
-        Duty Details: <strong>{worker.dutyDetails}</strong>
-      </h3>
-      <h3 className = "text-my-h3">
-        Email: <strong>{worker.email}</strong>
-      </h3>
+      <h3 className="text-my-h3">
+  <span className="text-my-h3-span">Position:</span> <strong>{worker.position}</strong>
+</h3>
+<h3 className="text-my-h3">
+  <span className="text-my-h3-span">Duty:</span> <strong>{worker.duty}</strong>
+</h3>
+<h3 className="text-my-h3">
+  <span className="text-my-h3-span">Field:</span> <strong>{worker.field}</strong>
+</h3>
+<h3 className="text-my-h3">
+  <span className="text-my-h3-span">Average Salary:</span> <strong>{worker.averageSalary}</strong>
+</h3>
+<h3 className="text-my-h3">
+  <span className="text-my-h3-span">Duty Details:</span> <strong>{worker.dutyDetails}</strong>
+</h3>
+<h3 className="text-my-h3">
+  <span className="text-my-h3-span">Email:</span> <strong>{worker.email}</strong>
+</h3>
     </div>
   );
 }
